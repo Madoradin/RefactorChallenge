cb69dbf4ba5b9b46747ea8ab5e706118774275f5
Owen Lin <lin.owen@gmail.com>
2023-01-10 14:53:47 -0800

    Refactor Player Controller
    Extract methods for movement, jump, rotation, camera rotation
    Convert hard-coded values in code to editable variables

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -7,34 +7,80 @@ namespace LongMethod
 {
     public class CharacterController : MonoBehaviour
     {
-        void Update()
-        {
-            // Keeps cursor locked in game window
-            Cursor.lockState = CursorLockMode.Locked;
+        //Declare movement modifiers and boundaries and make them editable so changing values does not require 
+        //digging into and changing functions
+        [SerializeField] private float moveSpeed = 7f;
+        [SerializeField] private float jumpSpeed = 10f;
 
-            var mov = (Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward) .normalized;
-            transform.position += mov * Time.deltaTime * 7;
+        //Declare min and max boundaries for the camera rotation and make them editable as well
+        [SerializeField] private float minRotate = 10f;
+        [SerializeField] private float maxRotate = 35f;
+        [SerializeField] private float offsetAngle = 45f;
 
-            if (Input.GetButtonDown("Jump"))
-                GetComponent<Rigidbody>().velocity = Vector3.up * 10;
-
-
-
-            transform.localRotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + 45 * Input.GetAxis("Mouse X") * Time.deltaTime, 0);
-
-            var t = transform.Find("Main Camera").transform;
-            var xRot = t.localRotation.eulerAngles.x - 45 * Input.GetAxis("Mouse Y") * Time.deltaTime;
-            if (xRot > 35) xRot = 35; else if (xRot < 10) xRot = 10;
-            t.localRotation = Quaternion.Euler(xRot, 0, 0);
-
-
-        }
+        //Declare frequently used Game Objects and initialize them on awake once so they aren't repeatedly done on every update
+        private Transform cameraTransform;
+        private Rigidbody rb;
 
         void Awake()
         {
+            rb = GetComponent<Rigidbody>();
+            cameraTransform = transform.Find("Main Camera").transform;
+        }
+
+        void Update()
+        {
+
+
+            //Split character movement into separate methods
+            MoveCharacter();
+            JumpCharacter();
+            RotateCharacter();
+
+            //Extract a method from the camera rotation code
+            RotateCamera();
 
         }
 
-        void Start() {}
+        //Handles character walking
+        void MoveCharacter()
+        {
+            var horizontal = Input.GetAxis("Horizontal");
+            var vertical = Input.GetAxis("Vertical");
+            var moveDirection = new Vector3(horizontal, 0, vertical);
+
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        }
+
+        //Handles character jumping
+        void JumpCharacter()
+        {
+            if (Input.GetButtonDown("Jump"))
+                rb.velocity = Vector3.up * jumpSpeed;
+        }
+
+        //handles character rotation
+        void RotateCharacter()
+        {
+            var rotationDirection = Input.GetAxis("Mouse X");
+            var rotatevalue = transform.rotation.eulerAngles.y + offsetAngle * rotationDirection * Time.deltaTime;
+            transform.localRotation = Quaternion.Euler(0, rotatevalue, 0);
+        }
+
+
+        //handles camera rotation
+        void RotateCamera()
+        {
+            var rotationDirection = cameraTransform.localRotation.eulerAngles.x - offsetAngle * Input.GetAxis("Mouse Y") * Time.deltaTime;
+            var xRotation = Mathf.Clamp(rotationDirection, minRotate, maxRotate);
+            cameraTransform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        }
+
+
+
+        void Start()
+        {
+            // Keeps cursor locked in game window
+            Cursor.lockState = CursorLockMode.Locked;
+        }
     }
 }
